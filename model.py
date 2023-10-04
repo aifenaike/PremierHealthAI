d020c876912db7e1f4a5d0a4d040368e3bd9a739
Sodiq179 <babawalesodiq996@gmail.com>
2023-10-05 00:38:06 +0100

    [Initial commit] model.py

---
+++ model.py
@@ -0,0 +1,68 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.prompts.prompt import PromptTemplate
+from langchain.chains import LLMChain, ConversationChain
+
+from state import state_store
+from model_templates import diagnosis_report_template, diffdiag_and_questions_template, diffdiag_template, questions_template
+
+
+## ===========================================================================================
+## ===========================================================================================
+## ===========================================================================================
+
+gpt3 = ChatOpenAI(
+    temperature=0.2,
+    streaming=True,
+    verbose=True)
+
+gpt4 = ChatOpenAI(model='gpt-4', temperature=0.2, streaming=True, verbose=True)
+
+## ===========================================================================================
+## ===========================================================================================
+## ===========================================================================================
+
+diagnosis_report_writer_template = PromptTemplate(
+    input_variables=["transcript", "pharmacist_hint"],
+    template = diagnosis_report_template)
+
+## ===========================================================================================
+## ===========================================================================================
+## ===========================================================================================
+
+diffdiag_and_questions_writer_template = PromptTemplate(
+    input_variables=["transcript"],
+    template = diffdiag_and_questions_template)
+
+## ===========================================================================================
+## ===========================================================================================
+## ===========================================================================================
+
+diffdiag_template_prompt = PromptTemplate(input_variables=["transcript"],
+                                       template= diffdiag_template)
+
+## ===========================================================================================
+## ===========================================================================================
+## ===========================================================================================
+
+questions_template_prompt = PromptTemplate(input_variables=["transcript"],
+                                      template=questions_template)
+
+
+## ===========================================================================================
+## ===========================================================================================
+## ===========================================================================================
+
+diffdiag_and_questions_writer = LLMChain(llm=gpt3, prompt=diffdiag_and_questions_writer_template, verbose=True)
+
+diffdiag_prompt = LLMChain(llm=gpt4, prompt= diffdiag_template_prompt, verbose=True)
+
+questions_prompt = LLMChain(llm=gpt4, prompt=questions_template_prompt, verbose=True)
+
+diagnosis_report_writer = LLMChain(llm=gpt4,
+                                prompt=diagnosis_report_writer_template,
+                                verbose=True)
+
+## ===========================================================================================
+## ===========================================================================================
+## ===========================================================================================
