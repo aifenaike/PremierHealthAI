2a9b4a7c01cf29e4924e48e978b93859b777b9dd
Sodiq179 <babawalesodiq996@gmail.com>
2023-10-17 02:01:44 +0100

    [Delete] Duplicate file

--- server/main.py
+++
@@ -1,6 +0,0 @@
-from app import start_socketio_server
-
-if __name__ == "__main__":
-    # cds_helper_ddx.run({"transcript": "hello world"}, callbacks=None)
-    print("[main] Running server thread")
-    start_socketio_server()
